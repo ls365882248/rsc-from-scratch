789143f41559fc3ed4c8be6200302ec4779bbe42
lihongliang <lihongliang@datagrand.com>
2023-08-09 15:34:41 +0800

    Step 6.3: Let's split the server in two

--- server/rsc.js
+++ server/rsc.js
@@ -1,25 +1,19 @@
 import { createServer } from "http";
 import { readFile, readdir } from "fs/promises";
-import { renderToString } from "react-dom/server";
 import sanitizeFilename from "sanitize-filename";
+
+// This is a server to host data-local resources like databases and RSC.
 
 createServer(async (req, res) => {
   try {
     const url = new URL(req.url, `http://${req.headers.host}`);
-    if (url.pathname === "/client.js") {
-      await sendScript(res, "./client.js");
-    } else if (url.searchParams.has("jsx")) {
-      url.searchParams.delete("jsx");
-      await sendJSX(res, <Router url={url} />);
-    } else {
-      await sendHTML(res, <Router url={url} />);
-    }
+    await sendJSX(res, <Router url={url} />);
   } catch (err) {
     console.error(err);
     res.statusCode = err.statusCode ?? 500;
     res.end();
   }
-}).listen(8080);
+}).listen(8081);
 
 function Router({ url }) {
   let page;
@@ -101,39 +95,11 @@ function Footer({ author }) {
   );
 }
 
-async function sendScript(res, filename) {
-  const content = await readFile(filename, "utf8");
-  res.setHeader("Content-Type", "text/javascript");
-  res.end(content);
-}
-
 async function sendJSX(res, jsx) {
   const clientJSX = await renderJSXToClientJSX(jsx);
   const clientJSXString = JSON.stringify(clientJSX, stringifyJSX);
   res.setHeader("Content-Type", "application/json");
   res.end(clientJSXString);
-}
-
-async function sendHTML(res, jsx) {
-  const clientJSX = await renderJSXToClientJSX(jsx);
-  let html = renderToString(clientJSX);
-  const clientJSXString = JSON.stringify(clientJSX, stringifyJSX);
-  html += `<script>window.__INITIAL_CLIENT_JSX_STRING__ = `;
-  html += JSON.stringify(clientJSXString).replace(/</g, "\\u003c");
-  html += `</script>`;
-  html += `
-    <script type="importmap">
-      {
-        "imports": {
-          "react": "https://esm.sh/react@canary",
-          "react-dom/client": "https://esm.sh/react-dom@canary/client"
-        }
-      }
-    </script>
-    <script type="module" src="/client.js"></script>
-  `;
-  res.setHeader("Content-Type", "text/html");
-  res.end(html);
 }
 
 function throwNotFound(cause) {
